6e61ae70d5724c78e0d9e4137f9a5956295948fb
abidkk <dev.abidkk@gmail.com>
2024-01-28 20:35:37 +0500

    updated

--- 01_python-module/mod1.py
+++ 01_python-module/mod1.py
@@ -1,2 +1,6 @@
 my_name = "Abid Khokhar"
 print(my_name)
+
+
+def hello(msg):
+    print(msg)
--- 01_python-module/mod2.py
+++ 01_python-module/mod2.py
@@ -1,6 +1,8 @@
-from mod1 import my_name # accessing my_name from mod1
+from mod1 import my_name, hello # accessing my_name from mod1
 
 print(my_name, "getting from mod1")
+
+hello("Hello from mod1") # accessing hello function from mod1
 
 # Note : In this way we can access anything from other files.
 # And you can see the folder (__pycache__) and file (mod1.cpython-312.pyc) created in it. 
